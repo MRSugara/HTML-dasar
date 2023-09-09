99b33cd42e1524e709e1a74a5f170f066393b01d
MRSugara <srobby109@gmail.com>
2023-09-09 21:32:35 +0700

    [fix]jsdasar.js

--- asset/js/jsdasar.js
+++ asset/js/jsdasar.js
@@ -8,6 +8,11 @@ console.log(umur)
 console.log(alamat)
 console.log(identitas);
 
+document.write(`<p>${nama}</p>`)
+document.write(`<p>${umur}</p>`)
+document.write(`<p>${alamat}</p>`)
+document.write(`<p>${identitas}</p>`)
+
 let sekolah = {
     nama : "SMAN 1 Lumajang",
     tahun: 2021,
@@ -15,19 +20,28 @@ let sekolah = {
 }
 console.log(sekolah);
 console.log(sekolah.alamat);
+document.write(`<p>${sekolah.alamat}</p>`)
+
 
 let angka = 10
 let angka2 = 2
 let penjumlahan = angka + angka2
 console.log(penjumlahan)
 console.log(typeof penjumlahan);
+document.write(`<p>${penjumlahan}</p>`)
+document.write(`<p>${typeof penjumlahan}</p>`)
+
 
 let penjumlahanToString = String(penjumlahan)
 console.log(penjumlahanToString)
 console.log(typeof penjumlahanToString);
+document.write(`<p>${penjumlahanToString}</p>`)
+document.write(`<p>${typeof penjumlahanToString}</p>`)
 
 for (let index = 0; index < 10; index++) {
     console.log(index)
+document.write(`<p>${index}</p>`)
+
     if (index == 5) {
         break
     }
