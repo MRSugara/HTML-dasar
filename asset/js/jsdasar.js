0cbcfa869e2f7f51d31ae1434aa13d8af30cc2f3
MRSugara <srobby109@gmail.com>
2023-09-09 21:10:24 +0700

    [FEAT] Landing,html

---
+++ asset/js/jsdasar.js
@@ -0,0 +1,28 @@
+let nama = "Mochammad Robby Sugara"
+var umur = 12
+const alamat = "Malang"
+
+let identitas = `nama saya ${nama}, umur saya ${umur}, saya sekaran tinggal di ${alamat}`
+console.log(nama)
+console.log(umur)
+console.log(alamat)
+console.log(identitas);
+
+let sekolah = {
+    nama : "SMAN 1 Lumajang",
+    tahun: 2021,
+    alamat:"Lumajang"
+}
+console.log(sekolah);
+console.log(sekolah.alamat);
+
+let angka = 10
+let angka2 = 2
+let penjumlahan = angka + angka2
+console.log(penjumlahan)
+console.log(typeof penjumlahan);
+
+let penjumlahanToString = String(penjumlahan)
+console.log(penjumlahanToString)
+console.log(typeof penjumlahanToString);
+
